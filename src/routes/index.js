9234fc8dd7d473d0e72ba8fd1dd9dcdfaf40d139
ThaiSonDev <sonthaidev91@gmail.com>
2023-05-14 23:45:39 +0700

    Api signUp

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("", (req, res, next) => {
-  return res.status(200).json({
-    message: "Welcome Backend Shopee",
-  });
-});
+router.use("/v1/api", require("./access"));
+
+// router.get("", (req, res, next) => {
+//   return res.status(200).json({
+//     message: "Welcome Backend Shopee",
+//   });
+// });
 
 module.exports = router;
