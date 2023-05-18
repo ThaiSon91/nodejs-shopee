45fe47cc637d52a3dc71c96af2bd837aefb1617c
ThaiSonDev <sonthaidev91@gmail.com>
2023-05-18 23:32:20 +0700

    Keytoken model

---
+++ src/models/keytoken.model.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//!dmbg
+const { model, Schema } = require("mongoose");
+
+const DOCUMENT_NAME = "Key";
+const COLLECTION_NAME = "Keys";
+
+// Declare the Schema of the mongo model
+const keytokenSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Shop",
+    },
+    publicKey: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true, collection: COLLECTION_NAME }
+);
+
+// Export the model
+module.exports = model(DOCUMENT_NAME, keytokenSchema);
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -40,6 +40,7 @@ class AccessService {
         const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
           modulusLength: 4096,
         });
+        console.log({ privateKey, publicKey });
       }
     } catch (error) {
       return {
