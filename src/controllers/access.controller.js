3ace16ae51543955320c09a185b5cbf4908592dc
ThaiSonDev <sonthaidev91@gmail.com>
2023-05-21 16:20:30 +0700

    Error Handler in API

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -4,21 +4,21 @@ const AccessService = require("../services/access.service");
 
 class AccessController {
   signUp = async (req, res, next) => {
-    try {
-      console.log(`[p]:::signUp`, req.body);
-      /*
+    // try {
+    //   console.log(`[p]:::signUp`, req.body);
+    /*
         200 OK
         201 CREATED
       */
-      // return res.status(201).json({
-      //   code: "20001",
-      //   metadata: { userid: 1 },
-      // });
+    // return res.status(201).json({
+    //   code: "20001",
+    //   metadata: { userid: 1 },
+    // });
 
-      return res.status(201).json(await AccessService.signUp(req.body));
-    } catch (error) {
-      next(error);
-    }
+    return res.status(201).json(await AccessService.signUp(req.body));
+    // } catch (error) {
+    //   next(error);
+    // }
   };
 }
 
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -8,6 +8,10 @@ const crypto = require("node:crypto");
 const KeyTokenService = require("./keyToken.service");
 const { createTokenPair } = require("../auth/authUtils");
 const { getInfoData } = require("../utils");
+const {
+  BadRequestError,
+  ConflictRequestError,
+} = require("../core/error.response");
 
 const RoleShop = {
   SHOP: "SHOP",
@@ -18,97 +22,98 @@ const RoleShop = {
 
 class AccessService {
   static signUp = async ({ name, email, password }) => {
-    try {
-      // step1: check email exists??
-      const holderShop = await shopModel.findOne({ email }).lean();
-      //.lean() tra ve mot object thuan tuy, ko dung lean() lon hon toi 30 lan
-      if (holderShop) {
+    // try {
+    // step1: check email exists??
+    const holderShop = await shopModel.findOne({ email }).lean();
+    //.lean() tra ve mot object thuan tuy, ko dung lean() lon hon toi 30 lan
+    if (holderShop) {
+      // return {
+      //   code: "xxx",
+      //   message: "Shop already registered",
+      // };
+      throw new BadRequestError("Error: Shop already registered");
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    //do mat khau phuc tap hon 10, thuat toan bam anh huong cpu nen dung dat nhieu qua
+    const newShop = await shopModel.create({
+      name,
+      email,
+      password: passwordHash,
+      roles: [RoleShop.SHOP],
+    });
+    // neu dang ky thanh cong cap cho refresh token va access token
+    // su dung 2 cai token truy cap he thong tao san pham luon
+    if (newShop) {
+      // create privatekey: cho nguoi dung (ko luu trong he thong), publicKey: luu trong he thong
+      // privatekey: sai cai token. publicKey: verify token
+      // Dung cho cac he thong lon nhu Amazon
+      // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+      //   modulusLength: 4096,
+      //   publicKeyEncoding: {
+      //     type: "pkcs1", //pkcs8
+      //     format: "pem",
+      //   },
+      //   privateKeyEncoding: {
+      //     type: "pkcs1", //pkcs8
+      //     format: "pem",
+      //   },
+      // });
+
+      // De don gian hon ko dung cryptogeneral nay
+      const privateKey = crypto.randomBytes(64).toString("hex");
+      const publicKey = crypto.randomBytes(64).toString("hex");
+
+      //Public Key CryptoGraphy Standards
+      console.log({ privateKey, publicKey }); // save collection KeyStore
+
+      const keyStore = await KeyTokenService.createKeyToken({
+        userId: newShop._id,
+        publicKey,
+        privateKey,
+      });
+
+      if (!keyStore) {
         return {
           code: "xxx",
-          message: "Shop already registered",
+          message: "keyStore error",
         };
       }
-
-      const passwordHash = await bcrypt.hash(password, 10);
-      //do mat khau phuc tap hon 10, thuat toan bam anh huong cpu nen dung dat nhieu qua
-      const newShop = await shopModel.create({
-        name,
-        email,
-        password: passwordHash,
-        roles: [RoleShop.SHOP],
-      });
-      // neu dang ky thanh cong cap cho refresh token va access token
-      // su dung 2 cai token truy cap he thong tao san pham luon
-      if (newShop) {
-        // create privatekey: cho nguoi dung (ko luu trong he thong), publicKey: luu trong he thong
-        // privatekey: sai cai token. publicKey: verify token
-        // Dung cho cac he thong lon nhu Amazon
-        // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-        //   modulusLength: 4096,
-        //   publicKeyEncoding: {
-        //     type: "pkcs1", //pkcs8
-        //     format: "pem",
-        //   },
-        //   privateKeyEncoding: {
-        //     type: "pkcs1", //pkcs8
-        //     format: "pem",
-        //   },
-        // });
-
-        // De don gian hon ko dung cryptogeneral nay
-        const privateKey = crypto.randomBytes(64).toString("hex");
-        const publicKey = crypto.randomBytes(64).toString("hex");
-
-        //Public Key CryptoGraphy Standards
-        console.log({ privateKey, publicKey }); // save collection KeyStore
-
-        const keyStore = await KeyTokenService.createKeyToken({
-          userId: newShop._id,
-          publicKey,
-          privateKey,
-        });
-
-        if (!keyStore) {
-          return {
-            code: "xxx",
-            message: "keyStore error",
-          };
-        }
-        /*publicKey chuyen hashstring luu vao database, 
+      /*publicKey chuyen hashstring luu vao database, 
         RSA ko luu truc tiep vao mongodb->chuyen json string luu vao db
         khi lay ra tu mongodb chuyen ve publicKey*/
 
-        // created token pair
-        const tokens = await createTokenPair(
-          { userId: newShop._id, email },
-          publicKey,
-          privateKey
-        );
-        console.log(`Created Token Success::`, tokens);
-
-        return {
-          code: 201,
-          metadata: {
-            shop: getInfoData({
-              fileds: ["_id", "name", "email"],
-              object: newShop,
-            }),
-            tokens,
-          },
-        };
-      }
+      // created token pair
+      const tokens = await createTokenPair(
+        { userId: newShop._id, email },
+        publicKey,
+        privateKey
+      );
+      console.log(`Created Token Success::`, tokens);
 
       return {
-        code: 200,
-        metadata: null,
-      };
-    } catch (error) {
-      return {
-        code: "xxx",
-        message: error.message,
-        status: "error",
+        code: 201,
+        metadata: {
+          shop: getInfoData({
+            fileds: ["_id", "name", "email"],
+            object: newShop,
+          }),
+          tokens,
+        },
       };
     }
+
+    return {
+      code: 200,
+      metadata: null,
+    };
+    // } catch (error) {
+    //   return {
+    //     code: "xxx",
+    //     message: error.message,
+    //     status: "error",
+    //   };
+    // }
   };
 }
 
